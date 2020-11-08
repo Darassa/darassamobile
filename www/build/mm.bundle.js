65860a236dde1cf7b1005b04157c681e6d4ab8bf
kamsdvd <kamsdvd@users.noreply.github.com>
2020-11-08 11:20:29 +0100

    url to lightsail

--- www/build/mm.bundle.js
+++ www/build/mm.bundle.js
@@ -71092,7 +71092,7 @@ angular.module('mm.addons.mod_workshop')
 angular.module('mm.core')
 
 .constant('mmCoreConfigConstants', {
-    "app_id" : "com.herokuapp.daracade.darassamobile",
+    "app_id" : "online.darassa.darassamobile",
     "appname": "Darassa Mobile",
     "desktopappname": "Darassa Desktop",
     "versioncode" : "2021",
@@ -71105,7 +71105,7 @@ angular.module('mm.core')
     "demo_sites": "",
     "gcmpn": "694767596569",
     "customurlscheme": "darassamobile",
-    "siteurl": "http://192.168.1.101/darassa.local",
+    "siteurl": "http://15.236.119.124/",
     "multisitesdisplay": "select",
     "skipssoconfirmation": "false",
     "forcedefaultlanguage": "false",
